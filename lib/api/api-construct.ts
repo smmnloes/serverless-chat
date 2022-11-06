dab7148817eff7ca97d45f2ce73ac4ddcf9a6d43
Maximilian Lösch <m.loesch@hotmail.de>
2022-11-06 15:32:40 +0100

    add handler function, websocket api

---
+++ lib/api/api-construct.ts
@@ -0,0 +1,24 @@
+import { WebSocketApi, WebSocketStage } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { WebSocketLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+
+export class ApiConstruct extends Construct {
+    constructor(scope: Construct, id: string) {
+        super(scope, id)
+
+        const defaultHandler = new NodejsFunction(this, 'default-handler')
+
+
+        const webSocketApi = new WebSocketApi(this, 'WebSocketApi', {
+            defaultRouteOptions: { integration: new WebSocketLambdaIntegration('DefaultIntegration', defaultHandler) }
+        })
+
+        new WebSocketStage(this, 'prod', {
+            webSocketApi,
+            stageName: 'prod',
+            autoDeploy: true,
+          });
+    }
+}
--- lib/serverless-chat-stack.ts
+++ lib/serverless-chat-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { ApiConstruct } from './api/api-construct';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ServerlessChatStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkappQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ApiConstruct(this, 'Api')
   }
 }
