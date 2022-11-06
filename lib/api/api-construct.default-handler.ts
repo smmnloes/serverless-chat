59e4048d9b92c3cca0e9e7d19c2e7464b5bcdd49
Maximilian Lösch <m.loesch@hotmail.de>
2022-11-06 17:45:46 +0100

    working imp of test client with websocket api

--- lib/api/api-construct.default-handler.ts
+++ lib/api/api-construct.default-handler.ts
@@ -1,3 +1,9 @@
-export async function handler(event: any){
+import { APIGatewayProxyWebsocketHandlerV2, APIGatewayProxyWebsocketEventV2, APIGatewayProxyResultV2 } from 'aws-lambda'
+
+export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event: APIGatewayProxyWebsocketEventV2): Promise<APIGatewayProxyResultV2> => {
     console.log(JSON.stringify(event, null, 4))
+    return {
+        statusCode: 200,
+        body: "hello client"
+    }
 }
