66294229220368e5d52af81ab09d9c337705a05a
Maximilian Lösch <maximilian.loesch@europace.de>
2022-11-19 12:15:29 +0100

    add messageId as list key prop

--- frontend/src/services/messageTransformer.tsx
+++ frontend/src/services/messageTransformer.tsx
@@ -3,5 +3,6 @@ import {RecieveMessageProps} from "../../../common/websocket-types/chat-message"
 
 export const messageTransformer = (message: RecieveMessageProps) => {
     const sentAtDate = new Date(message.sentAt);
-    return <li><b>{message.from}</b> <span>({sentAtDate.toLocaleDateString()}, {sentAtDate.toLocaleTimeString()})</span>: {message.message}</li>
+    return <li key={message.messageId}><b>{message.from}</b>
+        <span>({sentAtDate.toLocaleDateString()}, {sentAtDate.toLocaleTimeString()})</span>: {message.message}</li>
 }
