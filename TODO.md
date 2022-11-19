cc614af3cd0058bf73cf4b57ec48624ed5dffa62
Maximilian Lösch <maximilian.loesch@europace.de>
2022-11-19 15:21:26 +0100

    reconnect logic in websocket service

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 TODO:
 
 - Idle timeout increase or reconnect
-- messages table efficient query by latest messages
-  - limit rows to max amount by deleting all rows that exceed limit on getAllMessagesCall
 - retrieve latest messages on load
 - enter for input fields
--- frontend/src/services/websocket-service.ts
+++ frontend/src/services/websocket-service.ts
@@ -11,31 +11,56 @@ import {ConnectionStatus} from "../components/ChatView";
  * @param setConnectionStatus
  */
 export const websocketService = (url: string, userName: string, onmessage: (message: IMessageEvent) => void, onerror: (error: Error) => void, setWebsocketClient: React.Dispatch<React.SetStateAction<w3cwebsocket | null>>, setConnectionStatus: React.Dispatch<React.SetStateAction<ConnectionStatus>>): () => void => {
-    const client: w3cwebsocket = new w3cwebsocket(`${url}/?name=${userName}`)
-    client.onmessage = onmessage
-    client.onerror = onerror
-    client.onclose = () => console.log('Connection closed')
-    client.onopen = () => {
-        console.log('connected');
-        waitForConnection()
+    let currentReconnectAttempt = 0
+    const MAX_RECONNECT_ATTEMPTS = 3
+    let client: w3cwebsocket
 
-        // wait for connection
-        function waitForConnection() {
-            console.log('Waiting for client to be ready')
-            setConnectionStatus(ConnectionStatus.CONNECTING)
-            if (client.readyState === w3cwebsocket.OPEN) {
-                console.log('client ready! proceeding')
-                setConnectionStatus(ConnectionStatus.CONNECTED)
+    function clientSetup(): () => void {
+        client = new w3cwebsocket(`${url}/?name=${userName}`)
+
+        client.onmessage = onmessage
+        client.onerror = (error: Error) => {
+            onerror(error)
+            if (currentReconnectAttempt < MAX_RECONNECT_ATTEMPTS) {
+                currentReconnectAttempt++
+                console.log('trying to reconnect...attempt ' + currentReconnectAttempt)
+                clientSetup()
             } else {
-                setTimeout(waitForConnection, 1000)
+                console.log('Max retry attempts reached, doing nothing')
             }
         }
 
-        setWebsocketClient(client)
+        client.onclose = () => {
+            console.log('Connection closed');
+            setConnectionStatus(ConnectionStatus.DISCONNECTED)
+        }
+        client.onopen = () => {
+            console.log('connected');
+            waitForConnection()
+
+            // wait for connection
+            function waitForConnection() {
+                console.log('Waiting for client to be ready')
+                setConnectionStatus(ConnectionStatus.CONNECTING)
+                if (client.readyState === w3cwebsocket.OPEN) {
+                    console.log('client ready! proceeding')
+                    setConnectionStatus(ConnectionStatus.CONNECTED)
+                } else {
+                    setTimeout(waitForConnection, 1000)
+                }
+            }
+
+            setWebsocketClient(client)
+
+        }
+        // Cleanup function
+        return () => {
+            console.log('closing client')
+            client.close()
+        }
     }
-    // Cleanup function
-    return () => {
-        console.log('closing client')
-        client.close()
-    }
+
+    return clientSetup()
+
+
 }
