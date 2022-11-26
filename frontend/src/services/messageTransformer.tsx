d46f0e5f37ac3c2df76d362a74cb96ee9765d7ee
Maximilian Lösch <m.loesch@hotmail.de>
2022-11-26 12:32:55 +0100

    update message transformer

--- frontend/src/services/messageTransformer.tsx
+++ frontend/src/services/messageTransformer.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import {StoredMessageProps} from "../../../common/websocket-types/chat-message";
+import { StoredMessageProps } from "../../../common/websocket-types/chat-message";
 
 export const messageTransformer = (message: StoredMessageProps) => {
     const sentAtDate = new Date(message.sentAt);
-    return <li key={message.id}><b>{message.from}</b>
-        <span>({sentAtDate.toLocaleDateString()}, {sentAtDate.toLocaleTimeString()})</span>: {message.message}</li>
+    const dateTimeString = `${sentAtDate.getDate()}.${sentAtDate.getMonth() + 1}., ${pad(sentAtDate.getHours(), 2)}:${pad(sentAtDate.getMinutes(), 2)}`
+    return <li key={message.id}><b>{message.from} </b>
+        <span>({dateTimeString})</span>: {message.message}
+    </li>
+}
+
+function pad(num: number, size: number) {
+    let numString = num.toString();
+    while (numString.length < size) numString = "0" + numString;
+    return numString;
 }
